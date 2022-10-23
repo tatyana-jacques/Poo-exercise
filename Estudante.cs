2457ab2ae512ff80d4b0eda7da8c0ad209e85bef
Tatyana Jacques <tatyana_jacques@estudante.sesisenai.org.br>
2022-10-23 20:59:30 -0300

    Primeiro commit.

---
+++ Estudante.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exercicio_classe_estudante
+{
+    public class Estudante
+    {
+        private int _id;
+        private string _nome;
+        private string _exameMedico;
+        private DateTime _dataNascimento;
+        private bool _status;
+
+        public int Id {
+            get {return _id;}
+        }
+
+        public string Nome {
+            get {return _nome;}
+        }
+
+        public string ExameMedico {
+            get {return _exameMedico;}
+        }
+
+        public DateTime DataNascimento{
+            get {return _dataNascimento;}
+        }
+
+        public Estudante (int id, string nome, string exameMedico, DateTime dataNascimento){
+            _id = id;
+            _nome = nome;
+            _exameMedico = exameMedico;
+            _dataNascimento = dataNascimento;
+            _status = true;
+        }
+
+
+
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\n Hello, World!\n" +
+"\t Hello, hello! \t");
